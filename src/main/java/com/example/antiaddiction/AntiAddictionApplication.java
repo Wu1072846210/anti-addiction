a27b5b30cf96246a3ed26eae8cc627ba8282fd3d
binkylin <1072846210@qq.com>
2019-07-30 14:19:47 +0800

    try

---
+++ src/main/java/com/example/antiaddiction/AntiAddictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.antiaddiction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiAddictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiAddictionApplication.class, args);
+    }
+
+}
